ff0d98f26036024e2870b58671d6d2e63a6fd33a
AkiShi88 <barnabas.bernasek@gmail.com>
2021-12-25 11:49:29 +0100

    docs convert added params

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
  #this is the main file
   """Converts number (seconds) to seconds, minutes and hours.
+
+ Parameters
+ ----------
+ seconds : int
+     Number of seconds
  """
-  def convert(seconds):
-     seconds = seconds % (24 * 3600)
-     return "%d:%02d:%02d" % (hour, minutes, seconds)
